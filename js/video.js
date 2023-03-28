5175192dde32ebb3c9e063d98822f2c70ae265e6
aodemmon <83785343+aodemmon@users.noreply.github.com>
2023-03-28 18:26:11 -0400

    issues with play and push not registering button clicks

--- js/video.js
+++ js/video.js
@@ -1,11 +1,33 @@
 var video;
+var playBtn = document.getElementById("play");
+var pauseBtn = document.getElementById("pause");
+var slowerBtn = document.getElementById("slower");
+var fasterBtn = document.getElementById("faster");
+var skipBtn = document.getElementById("skip");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+window.addEventListener("click", function() {
+	console.log("Left click...");
+});
+
+if (pauseBtn) {
+	pauseBtn.addEventListener("click", function() {
+		console.log("Pause button pressed");
+	});
+};
+
+if (playBtn) {
+	playBtn.addEventListener("click", function() {
+		console.log("Play button pressed");
+	});
+};
+if (document.querySelector("#play")) {
+	document.querySelector("#play").addEventListener("click", function() {
+		console.log("Play Video");
+	});
+};
+
 
